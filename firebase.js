609227763822ecb370f32529c9472ffc4c00822f
franciscamarticorena-commits <francisca.marticorena@gmail.com>
2026-06-01 17:44:53 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDAGDPQXw_7IXfd1y_AyBAxm6pvZt0jOWM",
+  apiKey: "....",
   authDomain: "qinflo.firebaseapp.com",
   projectId: "qinflo",
   storageBucket: "qinflo.firebasestorage.app",
-  messagingSenderId: "662940889446",
-  appId: "1:662940889446:web:1e52dfebd1680ec62318a7"
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const db = firebase.firestore();  
+const db = firebase.firestore();
